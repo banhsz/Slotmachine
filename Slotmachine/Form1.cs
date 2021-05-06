12d4e8079bd37941f0147e85567815a47027898d
Kozik <kozikbro@gmail.com>
2021-05-06 16:02:16 +0200

    Symbols

--- Slotmachine/Form1.cs
+++ Slotmachine/Form1.cs
@@ -12,9 +12,34 @@ namespace Slotmachine
 {
     public partial class Form1 : Form
     {
+        private List<Symbol> symbols;
         public Form1()
         {
             InitializeComponent();
+            InitSymbols();
+        }
+
+        public void InitSymbols()
+        {
+            symbols = new List<Symbol>();
+            //commons
+            symbols.Add(new Symbol("ten", 0, 0.5, 5, 10));
+            symbols.Add(new Symbol("jack", 0, 0.5, 5, 12.5));
+            symbols.Add(new Symbol("queen", 0, 0.5, 5, 15));
+            symbols.Add(new Symbol("king", 0, 0.8, 6, 20));
+            symbols.Add(new Symbol("ace", 0, 0.8, 6, 25));
+
+            //rares
+            symbols.Add(new Symbol("blue", 0.2, 1, 7, 50));
+            symbols.Add(new Symbol("purple", 0.3, 1.2, 7.5, 75));
+            symbols.Add(new Symbol("pink", 0.4, 1.4, 8, 100));
+            symbols.Add(new Symbol("red", 0.5, 1.6, 9, 150));
+            symbols.Add(new Symbol("gold", 1, 2, 10, 500));
+
+            foreach (Symbol item in symbols)
+            {
+                Console.WriteLine(item.ToString());
+            }
         }
     }
 }
---
+++ Slotmachine/Symbol.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slotmachine
+{
+    class Symbol
+    {
+        private string name;
+        private double payout2;
+        private double payout3;
+        private double payout4;
+        private double payout5;
+
+        public string Name { get => name; }
+        public double Payout2 { get => payout2; }
+        public double Payout3 { get => payout3; }
+        public double Payout4 { get => payout4; }
+        public double Payout5 { get => payout5; }
+
+        public Symbol(string name, double payout2, double payout3, double payout4, double payout5)
+        {
+            this.name = name;
+            this.payout2 = payout2;
+            this.payout3 = payout3;
+            this.payout4 = payout4;
+            this.payout5 = payout5;
+        }
+
+        public override String ToString()
+        {
+            return String.Format("{0}\n{1} {2} {3} {4}\n",this.name,this.payout2,this.payout3,this.payout4,this.payout5);
+        }
+    }
+}
